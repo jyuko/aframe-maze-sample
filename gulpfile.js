b7211c949d962d0cbbfb71d7ab4dff6dd68eb841
jyuko <jyuko@hotmail.co.jp>
2016-12-15 23:32:57 +0900

    add gulp-uglify

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+
+gulp.task('js', function(){
+  browserify({
+    entries: ['index.js']
+  })
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(buffer())  
+  .pipe(uglify())
+  .pipe(rename({
+    extname: '.min.js'
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', ['js']);
