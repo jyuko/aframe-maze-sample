af597046779b368f75ba211dff86d8992b15ab40
jyuko <jyuko@hotmail.co.jp>
2016-12-19 22:03:20 +0900

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,37 @@ var rename = require('gulp-rename');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
+var browserSync = require('browser-sync');
 
-gulp.task('js', function(){
-  browserify({
-    entries: ['index.js']
-  })
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(buffer())  
-  .pipe(uglify())
-  .pipe(rename({
-    extname: '.min.js'
-  }))
-  .pipe(gulp.dest('dist/'));
+
+gulp.task('js', function () {
+    browserify({
+        entries: ['index.js']
+    })
+            .bundle()
+            .pipe(source('app.js'))
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(rename({
+                extname: '.min.js'
+            }))
+            .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['js']);
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: "./"
+            , index: "index.html"
+        }
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['js', 'browser-sync'], function () {
+    gulp.watch("./*.html", ['bs-reload']);
+    gulp.watch("./dist/*.js", ['bs-reload']);
+});
